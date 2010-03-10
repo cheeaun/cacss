17ddacba06d237d0e9ec1ad81f3074478f10206e
Lim Chee Aun <cheeaun@gmail.com>
2010-03-10 22:36:19 +0800

    Sizzling code by @rem and friends.

--- html5-ie.js
+++ html5-ie.js
@@ -2,8 +2,6 @@
 * more reference from: http://remysharp.com/2009/01/07/html5-enabling-script/
 */
  
-(function(){
-	var e = 'abbr article aside audio bb datagrid datalist details dialog eventsource figure footer header mark menu meter nav output progress section time video'.split(' ');
-	var i = e.length;
-	while (i--) document.createElement(e[i]);
-})();
+'abbr article aside audio canvas details figcaption figure footer header hgroup mark menu meter nav output progress section summary time video'.replace(/\w+/g,function(n){
+	document.createElement(n);
+});
